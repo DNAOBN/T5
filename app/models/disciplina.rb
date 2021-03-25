21d1b28f4e7d0877f93f4e2ae5f730792e7201c5
dobneves <dobneves@mppr.mp.br>
2021-03-24 23:41:54 -0300

    FIX: ApplicationRecord

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,6 +1,4 @@
-require 'active_record'
-
-class Disciplina < ActiveRecord::Base
+class Disciplina < ApplicationRecord
   has_and_belongs_to_many :discentes, -> { distinct }
   belongs_to :docente
   before_destroy { discentes.clear }
